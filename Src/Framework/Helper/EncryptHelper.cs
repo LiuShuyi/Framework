d5ef83cb63c7e03306315fb31d432ca7092fe1b0
LiuShuyi <liushuyi@live.cn>
2014-04-23 23:18:07 +0800

    Create EncryptHelper.cs

---
+++ Src/Framework/Helper/EncryptHelper.cs
@@ -0,0 +1,25 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Yintai.Service.Payment.TradeQuery.Util.Helper
+{
+    public static class EncrypHelper
+    {
+        /// <summary>
+        /// GetMD5
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="inputCharset"></param>
+        /// <returns></returns>
+        public static string GetMd5(string s, string inputCharset)
+        {
+            var buffer = new MD5CryptoServiceProvider().ComputeHash(Encoding.GetEncoding(inputCharset).GetBytes(s));
+            var builder = new StringBuilder(0x20);
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                builder.Append(buffer[i].ToString("x").PadLeft(2, '0'));
+            }
+            return builder.ToString();
+        }
+    }
+}
